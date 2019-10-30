f83c378fd9d641d85e0a0d201304e035083eb820
Victor Tran <victor.n.tran@outlook.com>
2019-10-29 20:51:38 -0700

    import Numbers

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Numbers from '../ButtonComponents/NumberButtons/Numbers'
 
 const Display = () => {
   return <div className="display">{/* Display any props data here */}</div>;
