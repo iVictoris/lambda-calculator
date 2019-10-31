1cbc53af4926d06d7209b5b314b4ba57753b8dae
Victor Tran <victor.n.tran@outlook.com>
2019-10-30 20:23:53 -0700

    get rid of numbers  and move into app

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,11 +1,9 @@
 import React from "react";
-import Numbers from "../ButtonComponents/NumberButtons/Numbers";
 
 const Display = () => {
   return (
     <div className="display">
       {/* Display any props data here */}
-      <Numbers />
     </div>
   );
 };
