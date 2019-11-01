c0fc99a978a60bee51396707a26ab18bcb77555c
Victor Tran <victor.n.tran@outlook.com>
2019-10-31 20:51:59 -0700

    add specialNumber class and style based on value of number

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const NumberButton = ({value, className}) => {
+  const newClassName = value === '0' ? 'specialNumber' : ''
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      <button className={className.concat(' numbers')}>
+      <button className={className.concat(` numbers ${newClassName}`)}>
       {value}
       </button>
     </>
