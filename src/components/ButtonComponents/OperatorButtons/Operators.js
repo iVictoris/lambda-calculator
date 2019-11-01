8bd0727dfcc7c46b4683198acc318f6161e501d1
Victor Tran <victor.n.tran@outlook.com>
2019-10-31 19:03:36 -0700

    export default Operators, format doc, render buttons

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,15 +1,14 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 //import any components needed
-import OperatorButton from './OperatorButton';
+import OperatorButton from "./OperatorButton";
 
 //Import your array data to from the provided data file
-import {operators} from '../../../data';
-import {createElements} from '../../../util/mapItems';
+import { operators } from "../../../data";
+import { createElements } from "../../../util/mapItems";
 
 const Operators = () => {
-
-  const [operatorChars, setOperatorChars] = useState(operators)
+  const [operatorChars, setOperatorChars] = useState(operators);
   const createOperatorButtons = createElements(OperatorButton);
 
   const buttons = operatorChars.map(createOperatorButtons);
@@ -20,6 +19,9 @@ const Operators = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
         component matching the name on the provided file. Pass
         it any props needed by the child component*/}
+        {buttons}
     </div>
   );
 };
+
+export default Operators;
