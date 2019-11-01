c0a9f91ce13c11f1ed3afe35c220048675e86485
Victor Tran <victor.n.tran@outlook.com>
2019-10-31 19:58:31 -0700

    change method structure and new helper method

--- src/util/mapItems.js
+++ src/util/mapItems.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 export const createElements = (ImportedComponent, acceptObjects = false) => {
-  if (acceptObjects)
-    return ({ value }) => <ImportedComponent value={value} key={value} />;
-  return value => <ImportedComponent value={value} key={value} />;
+  const defaultProps = {
+    value: "",
+    className: "btn",
+    key: ''
+  };
+  
+  if (acceptObjects) {
+    return ({value}) => {
+      const props = setProps(defaultProps, {value, key: value})
+
+      return <ImportedComponent {...props} />
+    }
+  }
+
+  return (value) => {
+    const props = setProps(defaultProps, {value, key: value})
+    return <ImportedComponent {...props} />
+  }
 };
+
+const setProps = (initialPropValues, valueToInsert) => {
+  return {
+    ...initialPropValues,
+    ...valueToInsert
+  }
+}
