42f2b955eb4582a196470c661bea68a185ec57a0
Victor Tran <victor.n.tran@outlook.com>
2019-10-31 18:24:51 -0700

    change number prop name to value

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const NumberButton = ({number}) => {
+const NumberButton = ({value}) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      {number}
+      {value}
     </>
   );
 };
