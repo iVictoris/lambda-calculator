2dda5855d94ffa66d3a0771ef6b34559b44da782
Victor Tran <victor.n.tran@outlook.com>
2019-10-31 18:22:47 -0700

    destructure props

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SpecialButton = (value) => {
+const SpecialButton = ({value}) => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
