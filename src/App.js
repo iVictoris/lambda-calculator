98261e4742650cefe32d449ee767fac2e55d5b5b
Victor Tran <victor.n.tran@outlook.com>
2019-10-31 18:20:32 -0700

    import specials, and render

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 import Logo from "./components/DisplayComponents/Logo";
 import Display from './components/DisplayComponents/Display';
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
+import Specials from './components/ButtonComponents/SpecialButtons/Specials';
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -22,6 +23,7 @@ function App() {
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
         <Display />
         <Numbers />
+        <Specials />
       </div>
     </div>
   );
